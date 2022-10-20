c9df0dbdf8452463d5948a780b29eb24d31ed6d1
lilian <lilian@algo.solutions>
2022-10-20 11:26:49 +0200

    1st commit of dev_python repository

---
+++ 1_main.py
@@ -0,0 +1,14 @@
+# monNombre1 = input("Entrez votre premier nombre : ")
+# monNombre2 = input("Entrez votre deuxième nombre : ")
+# monPremierNombre = int(monNombre1)
+# monNombreCaste = int(monNombre2)
+
+# if monPremierNombre > monNombreCaste: 
+#     print(f"Le nombre {monNombreCaste} est Superieur")
+# elif monNombreCaste == monPremierNombre:
+#     print(f"Tu as mis le même nombre ! => {monPremierNombre}")
+# else:
+#     print("null")
+
+# import random
+# print(random.randrange(1,1000))
