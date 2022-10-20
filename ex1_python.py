58fd53b590df58f7e3f19f9fde684943fd8c132a
lilian <lilian@algo.solutions>
2022-10-20 11:48:40 +0200

    Ajout de l'ex 1

---
+++ ex1_python.py
@@ -0,0 +1,25 @@
+# Variable "prenom" qui contient Pierre
+from operator import truediv
+
+prenom = "Pierre"
+print(prenom)
+
+# Variable "age" qui contient 20
+age = 20
+print(age)
+
+# Variable "majeur" qui contient booleen Vrai
+majeur = True
+print(majeur)
+
+# Variable "compte_en_banque" qui contient nombre décimal 20135,384
+compte_en_banque = float(20135.384)
+print(compte_en_banque)
+
+# Variable "amis" qui contient liste de trois chaîne Marie, Julien et Adrien
+amis = ["Marie", "Julien", "Adrien"]
+print(amis)
+
+# Variable "parents" qui contient un tuple avec 2 chaînes de caractères Marc et Caroline
+parents = ("Marc", "Caroline")
+print(parents)
